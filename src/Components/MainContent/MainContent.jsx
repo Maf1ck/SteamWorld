a3452bc0a750549adcdd63975d0d15f97772203d
maf1ck <kosmanmatvij@gmail.com>
2025-08-24 20:00:39 +0300

    add social links

--- src/Components/MainContent/MainContent.jsx
+++ src/Components/MainContent/MainContent.jsx
@@ -19,7 +19,10 @@ import {
   FaClipboardCheck,
   FaExclamationTriangle,
   FaPalette,
-  FaBan
+  FaBan,
+  FaYoutube,
+  FaDiscord,
+  FaTiktok
 } from 'react-icons/fa'
 
 const MainContent = () => {
@@ -121,6 +124,65 @@ const MainContent = () => {
                 <li><FaBan className={css.listIcon} /> Донат не мешает игре</li>
                 <li><FaBalanceScale className={css.listIcon} /> Все на равных</li>
               </ul>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      {/* Секция социальных сетей */}
+      <section className={css.socialSection}>
+        <div className={css.container}>
+          <h2 className={css.sectionTitle}>Присоединяйтесь к нам</h2>
+          <p className={css.sectionSubtitle}>
+            Следите за новостями сервера, общайтесь с игроками и смотрите интересный контент в наших социальных сетях
+          </p>
+          <div className={css.socialGrid}>
+            <div className={`${css.socialCard} ${css.youtubeCard}`}>
+              <div className={css.socialIcon}>
+                <FaYoutube />
+              </div>
+              <h3 className={css.socialTitle}>YouTube</h3>
+              <p className={css.socialDescription}>
+                Смотрите гайды по Create, обзоры сервера и забавные моменты игроков
+              </p>
+              <button 
+                className={`${css.socialButton} ${css.youtubeButton}`}
+                onClick={() => window.open('https://www.youtube.com/watch?v=3fSoaoQg0Ug', '_blank')}
+              >
+                ПОДПИСАТЬСЯ
+              </button>
+            </div>
+            
+            <div className={`${css.socialCard} ${css.discordCard}`}>
+              <div className={css.socialIcon}>
+                <FaDiscord />
+              </div>
+              <h3 className={css.socialTitle}>Discord</h3>
+              <p className={css.socialDescription}>
+                Присоединяйтесь к нашему сообществу, общайтесь и получайте поддержку
+              </p>
+              <button 
+                className={`${css.socialButton} ${css.discordButton}`}
+                onClick={() => window.open('https://discord.gg/G2Ttb8Gj7x', '_blank')}
+              >
+                ПРИСОЕДИНИТЬСЯ
+              </button>
+            </div>
+            
+            <div className={`${css.socialCard} ${css.tiktokCard}`}>
+              <div className={css.socialIcon}>
+                <FaTiktok />
+              </div>
+              <h3 className={css.socialTitle}>TikTok</h3>
+              <p className={css.socialDescription}>
+                Короткие видео с сервера, лайфхаки и забавные моменты в игре
+              </p>
+              <button 
+                className={`${css.socialButton} ${css.tiktokButton}`}
+                onClick={() => window.open('https://www.tiktok.com/@structo0', '_blank')}
+              >
+                ПОДПИСАТЬСЯ
+              </button>
             </div>
           </div>
         </div>
